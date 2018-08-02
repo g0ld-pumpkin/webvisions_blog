d527b652e62bdba17edf550fa5ace09c9cd9e16b
Ruby Dev <rubyplayer@Rubys-iMac.local>
2018-08-02 20:33:58 +0800

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  layout :for_devise
+  def for_devise
+    'author'
+  end
+end
---
+++ app/controllers/blog/pages_controller.rb
@@ -0,0 +1,12 @@
+module Blog
+  class PagesController < BlogController
+
+    def about
+      #code
+    end
+
+    def contact
+      #code
+    end
+  end
+end
